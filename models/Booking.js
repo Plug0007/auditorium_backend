4e308a93ac2ad13acdc5227828c24d9786b24afb
Raelyaan plug <plug9999999@gmail.com>
2025-03-13 20:23:07 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -6,7 +6,7 @@ const Booking = bookingDB.define('Booking', {
   facultyId: { type: DataTypes.INTEGER, allowNull: false },
   eventName: { type: DataTypes.STRING, allowNull: false },
   coordinator: { type: DataTypes.STRING, allowNull: false },
-  coordinatorContact: { type: DataTypes.INTEGER, allowNull: true },
+  coordinatorContact: { type: DataTypes.STRING, allowNull: true },
   eventType: { type: DataTypes.STRING, allowNull: false },
   date: { type: DataTypes.STRING, allowNull: false },
   startTime: { type: DataTypes.STRING, allowNull: false },
